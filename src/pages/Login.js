d843bf32cef276924a6f19addb182a7c4288ebbc
mangelpt <mg20101994@gmail.com>
2021-10-28 23:51:19 -0500

    loginPage

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,57 @@
-const Login = () => {
-  return <h1>Login</h1>
-}
+import styled from "@emotion/styled";
+import React from "react";
+import { Container } from "../components/UI/Container";
 
-export default Login
+const LoginContainer = styled.div`
+  margin: auto;
+  height: 600px;
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  h2 {
+    font-size: 24px;
+    color: #4f4f4f;
+    align-self: flex-start;
+    margin-left: 45px;
+  }
+  p {
+    margin: 0;
+    font-size: 48px;
+    font-weight: bold;
+    span {
+      background-color: #4f4f4f;
+      color: #fff;
+      padding: 2px;
+      border-radius: 10px;
+    }
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 10px;
+  width: inherit;
+  gap: 15px;
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <LoginContainer>
+        <h2> Welcome to ....</h2>
+        <p>
+          Linke <span>able</span>
+        </p>
+        <ButtonContainer>
+          <button>Continue as Guest</button>
+          <button>Login</button>
+        </ButtonContainer>
+      </LoginContainer>
+    </Container>
+  );
+};
+export default Login;

4e02887442ede9d3d40f7d06ea6004d02b0f08d2
ChristianMRtz <christianmr268@gmail.com>
2021-10-28 23:44:42 -0500

    Added Component Form and page Personal Information

---
+++ src/components/Forms.js
@@ -0,0 +1,5 @@
+import styled from "@emotion/styled";
+
+export const TitleForm = styled.h1`
+
+`;
---
+++ src/pages/PersonalInformationForm.js
@@ -0,0 +1,11 @@
+import { TitleForm } from "../components/Forms"
+
+const PersonalInformationForm = () => {
+  return (
+    <div>
+      <TitleForm>Create a new Candidate</TitleForm>
+    </div>
+  )
+}
+
+export default PersonalInformationForm

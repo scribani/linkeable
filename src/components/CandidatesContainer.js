8169d859c56077ecc1bf834f0fb26c8c83c3df05
Scribani <scribani.andres@gmail.com>
2021-10-29 18:23:54 -0500

    Create CandidatesContainer component

---
+++ src/components/CandidatesContainer.js
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export default styled.ul`
+  list-style: none;
+  width: 315px;
+  height: 516px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+  overflow-y: auto;
+`;

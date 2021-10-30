deac46da6600c01f7f586cbce3ccc59c7bf14b22
Scribani <scribani.andres@gmail.com>
2021-10-30 11:42:24 -0500

    Create useSubmitable hook to manage form status

---
+++ src/hooks/useSubmitable.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export default function useSubmitable(form, requiredFields) {
+  const [submitable, setSubmitable] = useState(false);
+  const requiredData = requiredFields.map((field) => form[field]);
+
+  useEffect(() => {
+    if (requiredData.every((field) => field)) {
+      setSubmitable(true);
+    } else {
+      setSubmitable(false);
+    }
+  }, [form]);
+
+  return submitable;
+}
